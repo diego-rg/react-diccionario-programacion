23497b1eff8fb916cc335ff7cefede4bbb6a989a
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-05-12 20:58:05 +0200

    intro section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,39 +2,75 @@ import React from "react";
 
 import LinkNav from "./LinkNav";
 
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography, Grid, Button } from "@mui/material";
 import { Card, CardContent, CardMedia, CardActionArea } from "@mui/material";
 
 const Home = () => {
   return (
     <Box component="main" sx={{ marginY: 8 }}>
       <Grid spacing={9} container alignItems="center" justifyContent="center">
-        <Grid item xs={12} sm={6} md={6}>
+        <Grid item xs={12} sm={12} md={6}>
           <Typography
             variant="body1"
             component="p"
             sx={{
-              marginX: "auto",
-              maxWidth: {md: "75%"},
+              margin: "auto",
+              maxWidth: { md: "70%" },
               textAlign: { xs: "center", sm: "center", md: "start" },
             }}
           >
             Consulta el diccionario de programación, crea tus propios apuntes en
             formato doc o consume nuestra API REST en tus proyectos.
           </Typography>
+          <Box
+            sx={{
+              margin: "auto",
+              maxWidth: { md: "70%" },
+              textAlign: { xs: "center", sm: "center", md: "start" },
+            }}
+          >
+            <Button
+              sx={{
+                marginY: 2,
+                fontSize: {
+                  xs: "0.8rem",
+                  sm: "1.05rem",
+                },
+              }}
+              size="large"
+              variant="contained"
+              color="primary"
+              href="#info"
+            >
+              Comenzar
+            </Button>
+          </Box>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={6}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
           <Box
             component="img"
             src="/book.svg"
-            width="75%"
+            width="70%"
             alt="Imagen de una mujer con un libro"
           ></Box>
         </Grid>
       </Grid>
 
-      <Grid spacing={9} container>
+      <Grid
+        id="info"
+        spacing={9}
+        container
+        sx={{ marginY: 4 }}
+        alignItems="center"
+        justifyContent="center"
+      >
         <Grid item xs={12} sm={6} md={4}>
           <LinkNav href="/dictionary">
             <Card
