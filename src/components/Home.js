1b460099dd673367e4e3f8a8005fd558cd353a2c
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-05-22 14:26:07 +0200

    intro section

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,32 +18,41 @@ const Home = () => {
     >
       <Grid
         component="section"
-        spacing={9}
         container
         alignItems="center"
         justifyContent="center"
         maxWidth="md"
       >
-        <Grid item xs={12} sm={12} md={6}>
-          <Typography
-            variant="body1"
-            component="p"
-            sx={{
-              margin: "auto",
-              maxWidth: "500px",
-              textAlign: { xs: "center", sm: "center", md: "start" },
-            }}
-          >
-            Consulta el diccionario de programación, crea tus propios apuntes en
-            formato doc o consume nuestra API REST en tus proyectos.
-          </Typography>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          sx={{
+            padding: 4,
+            minHeight: "50vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <Box
             sx={{
-              margin: "auto",
               maxWidth: "500px",
               textAlign: { xs: "center", sm: "center", md: "start" },
             }}
           >
+            <Typography
+              variant="body1"
+              component="p"
+              sx={{
+                textAlign: { xs: "center", sm: "center", md: "start" },
+              }}
+            >
+              Consulta el diccionario de programación, crea tus propios apuntes
+              en formato doc o consume nuestra API REST en tus proyectos.
+            </Typography>
             <Button
               sx={{
                 marginY: 2,
@@ -67,6 +76,7 @@ const Home = () => {
           sm={12}
           md={6}
           sx={{
+            padding: 4,
             display: "flex",
             justifyContent: "center",
             order: { xs: -1, sm: -1, md: 1 },
@@ -75,7 +85,7 @@ const Home = () => {
           <Box
             component="img"
             src="/book.svg"
-            width="90%"
+            width="100%"
             maxWidth="400px"
             alt="Imagen de una mujer con un libro"
           ></Box>
