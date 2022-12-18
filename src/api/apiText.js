147091f1892c1c5aa98e84a6c2fe32fd1a9eef0d
eu <diego_thegarden@hotmail.com>
2022-12-18 19:43:41 +0100

    migration from vercel to railway

--- src/api/apiText.js
+++ src/api/apiText.js
@@ -2,32 +2,32 @@ const apiText = [
   {
     title: "Término aleatorio:",
     description:
-      "https://api-diccionarioprogramacion.herokuapp.com/api/terms/random",
+      "https://api-diccionario-programacion.up.railway.app/terms/random",
   },
   {
     title: "Todos los términos:",
     description:
-      "https://api-diccionarioprogramacion.herokuapp.com/api/terms/all",
+      "https://api-diccionario-programacion.up.railway.app/terms/all",
   },
   {
     title: "Categorías existentes:",
     description:
-      "https://api-diccionarioprogramacion.herokuapp.com/api/terms/categories",
+      "https://api-diccionario-programacion.up.railway.app/terms/categories",
   },
   {
     title: "Términos de una categoría:",
     description:
-      "https://api-diccionarioprogramacion.herokuapp.com/api/terms/{category}",
+      "https://api-diccionario-programacion.up.railway.app/terms/{category}",
   },
   {
     title: "Término concreto:",
     description:
-      "https://api-diccionarioprogramacion.herokuapp.com/api/terms/{category}/{name}",
+      "https://api-diccionario-programacion.up.railway.app/terms/{category}/{name}",
   },
   {
     title: "Búsqueda:",
     description:
-      "https://api-diccionarioprogramacion.herokuapp.com/api/search?q={query}",
+      "https://api-diccionario-programacion.up.railway.app/search?q={query}",
   },
 ];
 
--- src/api/dictionary.js
+++ src/api/dictionary.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 //Función auxiliar con la url base a la que haremos requests con axios
 export default axios.create({
-  baseURL: "https://api-diccionarioprogramacion.herokuapp.com/api",
+  baseURL: "https://api-diccionario-programacion.up.railway.app/api",
 });
