c3f1396c6860d668aa453826a958b83e34c34700
eu <diego_thegarden@hotmail.com>
2022-12-18 19:48:04 +0100

    migration from vercel to railway2

--- src/api/apiText.js
+++ src/api/apiText.js
@@ -2,32 +2,32 @@ const apiText = [
   {
     title: "Término aleatorio:",
     description:
-      "https://api-diccionario-programacion.up.railway.app/terms/random",
+      "https://api-diccionario-programacion.up.railway.app/api/terms/random",
   },
   {
     title: "Todos los términos:",
     description:
-      "https://api-diccionario-programacion.up.railway.app/terms/all",
+      "https://api-diccionario-programacion.up.railway.app/api/terms/all",
   },
   {
     title: "Categorías existentes:",
     description:
-      "https://api-diccionario-programacion.up.railway.app/terms/categories",
+      "https://api-diccionario-programacion.up.railway.app/api/terms/categories",
   },
   {
     title: "Términos de una categoría:",
     description:
-      "https://api-diccionario-programacion.up.railway.app/terms/{category}",
+      "https://api-diccionario-programacion.up.railway.app/api/terms/{category}",
   },
   {
     title: "Término concreto:",
     description:
-      "https://api-diccionario-programacion.up.railway.app/terms/{category}/{name}",
+      "https://api-diccionario-programacion.up.railway.app/api/terms/{category}/{name}",
   },
   {
     title: "Búsqueda:",
     description:
-      "https://api-diccionario-programacion.up.railway.app/search?q={query}",
+      "https://api-diccionario-programacion.up.railway.app/api/search?q={query}",
   },
 ];
 
